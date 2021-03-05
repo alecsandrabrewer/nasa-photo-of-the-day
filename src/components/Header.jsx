1bf3b8371b5bf9850b883c8e2eac716f75319e11
Alecsandra Brewer <alecsandrabrewer@yahoo.com>
2021-03-04 19:48:03 -0800

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavStyled = styled.nav`
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #b4ceff;
+    width: 80%;
+    margin: 0 auto;
+    .copyright {
+        font-style:italic;
+        font-size: 1.3rem;
+        color:#6d6d6e;
+    }
+`;
 
 const Header = (props) => {
  
     const {copyright, title} = props.data
     return (
-        <nav>
+        <NavStyled>
             <h1>
                 NASA Photo of the Day
             </h1>
             <h2>
             {title} <span className='copyright'> by {copyright}</span>
             </h2>
-        </nav>
+        </NavStyled>
     )
 }
 
